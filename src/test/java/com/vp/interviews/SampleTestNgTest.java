67979644977bf9b2dd0d0642f5892e5ee801da3f
ajaramillo <andersonjaramillo@eharmony.com>
2017-09-19 11:20:25 -0700

    Fix issues on test

--- src/test/java/com/vp/interviews/SampleTestNgTest.java
+++ src/test/java/com/vp/interviews/SampleTestNgTest.java
@@ -6,10 +6,6 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.vp.interviews.pages.HomePage;
-
 public class SampleTestNgTest extends TestNgTestBase {
-
-  private HomePage homepage;
 
 }
