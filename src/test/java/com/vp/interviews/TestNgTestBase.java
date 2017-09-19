1ca006000e0a43cc52382c4b39b52ad9378bc6af
Anderson Jaramillo <jaraudea@gmail.com>
2017-09-19 00:19:25 -0500

    Initial project

---
+++ src/test/java/com/vp/interviews/TestNgTestBase.java
@@ -0,0 +1,48 @@
+package com.vp.interviews;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Capabilities;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import ru.stqa.selenium.factory.WebDriverPool;
+
+/**
+ * Base class for TestNG-based test classes
+ */
+public class TestNgTestBase {
+
+  protected static URL gridHubUrl = null;
+  protected static String baseUrl;
+  protected static Capabilities capabilities;
+
+  protected WebDriver driver;
+
+  @BeforeSuite
+  public void initTestSuite() throws IOException {
+    SuiteConfiguration config = new SuiteConfiguration();
+    baseUrl = config.getProperty("site.url");
+    if (config.hasProperty("grid.url") && !"".equals(config.getProperty("grid.url"))) {
+      gridHubUrl = new URL(config.getProperty("grid.url"));
+    }
+    capabilities = config.getCapabilities();
+  }
+
+  @BeforeMethod
+  public void initWebDriver() {
+    System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver");
+    System.setProperty("webdriver.gecko.driver", "drivers/geckodriver-v0.17.0-linux64/geckodriver");
+
+    driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    WebDriverPool.DEFAULT.dismissAll();
+  }
+}
