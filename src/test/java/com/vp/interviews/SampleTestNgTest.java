44bca863915a4f101a109fb5365b250ec92cd6ad
ajaramillo <andersonjaramillo@eharmony.com>
2017-09-19 11:23:33 -0700

    Fix issue on tests

--- src/test/java/com/vp/interviews/SampleTestNgTest.java
+++ src/test/java/com/vp/interviews/SampleTestNgTest.java
@@ -1,11 +1,5 @@
 package com.vp.interviews;
 
-import org.openqa.selenium.support.PageFactory;
-
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 public class SampleTestNgTest extends TestNgTestBase {
 
 }
