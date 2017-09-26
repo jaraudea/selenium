38c5d716477ac92658e9d8fb0457ca9430eab48b
ajaramillo <andersonjaramillo@eharmony.com>
2017-09-26 10:26:12 -0700

    Fix issue on tests

--- src/test/java/com/vp/interviews/SampleTestNgTest.java
+++ src/test/java/com/vp/interviews/SampleTestNgTest.java
@@ -1,5 +1,30 @@
 package com.vp.interviews;
 
+import org.testng.annotations.*;
+
 public class SampleTestNgTest extends TestNgTestBase {
+
+
+    @BeforeClass
+    public void setup() {
+
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+    }
+
+    @Test
+    public void testAviancaBooking() {
+        driver.get(baseUrl);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+    }
+
+    @AfterClass
+    public void cleanUp() {
+    }
 
 }
